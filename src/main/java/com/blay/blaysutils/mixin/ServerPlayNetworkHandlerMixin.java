6bdbeeb1c90dcd3c2a26a2b2af29bea88d9ef4cd
Wojciech Paruch <gpewojan1@gmail.com>
2023-05-15 18:47:19 +0200

    The mixin's almost finished, I have no way to test or compile it rn

--- src/main/java/com/blay/blaysutils/mixin/ServerPlayNetworkHandlerMixin.java
+++ src/main/java/com/blay/blaysutils/mixin/ServerPlayNetworkHandlerMixin.java
@@ -1,9 +1,19 @@
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import net.minecraft.server.network.ServerPlayNetworkHandler;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.network.ClientConnection;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
 
 @Mixin(ServerPlayNetworkHandler.class)
 public class ServerPlayNetworkHandlerMixin {
-	@Inject(at = @At("HEAD"), method = "init()V")
-	private void init(CallbackInfo info) {
-		System.out.println("This line is printed by an example mod mixin!");
+	@Inject(at = @At("TAIL"), method = "Lnet/minecraft/server/network/ServerPlayNetworkHandler;<init>(Lnet/minecraft/server/MinecraftServer;Lnet/minecraft/network/ClientConnection;Lnet/minecraft/server/network/ServerPlayerEntity;)V")
+	private void inject(CallbackInfo ci, MinecraftServer server, ClientConnection connection, ServerPlayerEntity player) {
+        System.out.println("test");
+        player.sendMessage(Text.literal("Hello, biyach!"));
 	}
 }
